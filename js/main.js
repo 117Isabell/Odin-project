6275f3784172ba9fa0bd79c752709d0b16e0c66b
117Isabell <isabellhepburn@outlook.com>
2022-11-30 22:13:31 +0100

    add new files

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let x = 23;
+let y = 97
+console.log(x + y)
+
+console.log(1 + 2 + 3 + 4 + 5 + 6)
+
+console.log((4 + 6 + 9) / 77)
+
+// returns undefined
+// let b = 7 * a;
+// console.log(b)
+
+
+let max = 57;
+actual = max - 13;
+percentage = actual / max;
+
+
+const myNum2 = 123;
+const myString2 = myNum2.toString();
+console.log(typeof myString2);
+
+//true
+console.log(5 > 4) 
+//false
+console.log("apple" > "pineapple")
+//true, string2>1
+console.log("2" > "12")
+//true
+console.log(undefined == null)
+//false
+console.log(undefined === null)
+//flase
+console.log(null == "\n0\n")
+//flase
+console.log(null === +"\n0\n")
